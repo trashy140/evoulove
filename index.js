8c761104934c66bfc18edfce7d7499062989a60e
trashy140 <samorTrash@hotmail.com>
2024-10-01 00:45:07 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -4,3 +4,18 @@ const heart = document.querySelector('.heart');
 heart.addEventListener('click', () => {
     envelope.classList.toggle('flap');
 });
+
+
+function setRealVh() {
+    // Calcul de la vraie hauteur du viewport
+    let vh = window.innerHeight * 0.01;
+    // Appliquer cette valeur en tant que hauteur personnalisée
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+}
+
+// Appeler la fonction à chaque changement de taille
+window.addEventListener('resize', setRealVh);
+window.addEventListener('orientationchange', setRealVh);
+
+// Appel initial
+setRealVh();
